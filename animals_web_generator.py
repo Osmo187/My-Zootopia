0b1be27cc4ff45f096bb4c1a01c4ed2f3adffedb
Osmo187 <i.schuette22@googlemail.com>
2026-04-16 11:08:28 +0200

    HTML für Tiere generiert

---
+++ animals_web_generator.py
@@ -0,0 +1,58 @@
+import json
+
+
+def load_data(file_path):
+    """ Loads a JSON file """
+    with open(file_path, "r") as handle:
+        return json.load(handle)
+
+
+def generate_animal_html(animal):
+    """ Generates HTML for a single animal """
+    characteristics = animal.get("characteristics", {})
+
+    name = animal.get("name", "Unknown")
+    diet = characteristics.get("diet", "Unknown")
+    location = animal.get("locations", ["Unknown"])[0]
+    animal_type = characteristics.get("type", "Unknown")
+
+    html = f"""
+    <li class="cards__item">
+        <div class="card__title">{name}</div>
+        <p class="card__text">
+            Diet: {diet}<br>
+            Location: {location}<br>
+            Type: {animal_type}
+        </p>
+    </li>
+    """
+
+    return html
+
+
+def load_template(file_path):
+    """ Loads HTML template """
+    with open(file_path, "r") as file:
+        return file.read()
+
+
+def main():
+    animals_data = load_data("animals_data.json")
+
+    animals_html = ""
+
+    for animal in animals_data:
+        animals_html += generate_animal_html(animal)
+
+    template = load_template("animals_template.html")
+
+    final_html = template.replace("__REPLACE_ANIMALS_INFO__", animals_html)
+
+    with open("animals.html", "w") as file:
+        file.write(final_html)
+
+    print("HTML file successfully generated: animals.html")
+
+
+if __name__ == "__main__":
+    main()
