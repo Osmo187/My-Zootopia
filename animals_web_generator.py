ec44613ac83f9ced3ff18aec5465b83850d2d400
Osmo187 <i.schuette22@googlemail.com>
2026-04-16 12:17:12 +0200

    verbessertes HTML & CSS

--- animals_web_generator.py
+++ animals_web_generator.py
@@ -1,61 +1,25 @@
-import json
-
-
-def load_data(file_path):
-    """ Loads a JSON file """
-    with open(file_path, "r") as handle:
-        return json.load(handle)
-
-
 def generate_animal_html(animal):
-    """ Generates HTML for a single animal """
     characteristics = animal.get("characteristics", {})
 
     name = animal.get("name", "Unknown")
     diet = characteristics.get("diet", "Unknown")
     location = animal.get("locations", ["Unknown"])[0]
-    type_html = ""
 
+    type_html = ""
     if "type" in characteristics:
-        type_html = f"Type: {characteristics['type']}<br>"
+        type_html = f"<li><strong>Type:</strong> {characteristics['type']}</li>"
 
     html = f"""
     <li class="cards__item">
         <div class="card__title">{name}</div>
-        <p class="card__text">
-            <strong>Diet:</strong> {diet}<br>
-            <strong>Location:</strong> {location}<br>
-            {type_html}
-        </p>
+        <div class="card__text">
+            <ul class="animal-details">
+                <li><strong>Diet:</strong> {diet}</li>
+                <li><strong>Location:</strong> {location}</li>
+                {type_html}
+            </ul>
+        </div>
     </li>
     """
 
     return html
-
-
-def load_template(file_path):
-    """ Loads HTML template """
-    with open(file_path, "r") as file:
-        return file.read()
-
-
-def main():
-    animals_data = load_data("animals_data.json")
-
-    animals_html = ""
-
-    for animal in animals_data:
-        animals_html += generate_animal_html(animal)
-
-    template = load_template("animals_template.html")
-
-    final_html = template.replace("__REPLACE_ANIMALS_INFO__", animals_html)
-
-    with open("animals.html", "w") as file:
-        file.write(final_html)
-
-    print("HTML file successfully generated: animals.html")
-
-
-if __name__ == "__main__":
-    main()
